e42a29f895981ac1843d9d7669eb3470f782826a
Abzed <ibraabzed@gmail.com>
2022-09-11 08:23:11 +0300

    commit tree b2e609e77918b4705b590e3cfebddc796fc049c2

---
+++ db/migrate/20220823130101_create_students_table.rb
@@ -0,0 +1,11 @@
+class CreateStudentsTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :auditions do |t|
+        t.string :actor
+        t.string :location
+        t.integer :phone
+        t.integer :hired
+        t.integer :role_id
+    end        
+  end
+end
